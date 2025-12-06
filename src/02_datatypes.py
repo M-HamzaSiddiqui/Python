28ff461d2af483e8f12a75a9895c8afbd189a1de
Mohd Hamza Siddiqui <hamza.siddiqui@sharajman.com>
2025-12-06 12:35:41 +0530

    mutability and immutability in python

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -1,1 +1,14 @@
 print('hello, to python')
+
+a=2
+
+print(f'a is {a}')
+print(f'id of 2 is {id(a)}')
+
+
+a= 20
+print(f'a is {a}')
+print(f'id of 20 is {id(a)}')
+
+# the no. are immutable and they both have different identity
+
